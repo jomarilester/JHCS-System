bed2692461b5be5551481435c3b3139042f74104
aeneid415 <aeneid415@hotmail.com>
2018-02-09 02:07:28 +0800

    Made the font of two report pages smaller for easier viewing. Also added the tentative final database, as there are some clarifications needed in regard to the user interface (see concerns.txt).

---
+++ jhcs_new/jhcs_new.sql
@@ -0,0 +1,274 @@
+CREATE DATABASE  IF NOT EXISTS `jhcs_new` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `jhcs_new`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jhcs_new
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blends`
+--
+
+DROP TABLE IF EXISTS `blends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blends` (
+  `blend_id` int(11) NOT NULL,
+  `item_id` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `unitprice` int(11) DEFAULT NULL,
+  `client_id` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`blend_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blends`
+--
+
+LOCK TABLES `blends` WRITE;
+/*!40000 ALTER TABLE `blends` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blends` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `client_id` varchar(5) NOT NULL,
+  `client_name` varchar(90) NOT NULL,
+  `client_type` enum('Wholesale','Retail') NOT NULL,
+  `contact_personnel` varchar(90) NOT NULL,
+  `position` varchar(35) NOT NULL,
+  `address` varchar(140) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telephone_no` varchar(10) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `coffee_raw`
+--
+
+DROP TABLE IF EXISTS `coffee_raw`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `coffee_raw` (
+  `rawcoffee_id` int(11) NOT NULL,
+  `item_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`rawcoffee_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coffee_raw`
+--
+
+LOCK TABLES `coffee_raw` WRITE;
+/*!40000 ALTER TABLE `coffee_raw` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coffee_raw` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(45) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `unit_price` int(11) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `machine_client`
+--
+
+DROP TABLE IF EXISTS `machine_client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `machine_client` (
+  `machine_serial` int(11) NOT NULL,
+  `machine_id` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`machine_serial`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `machine_client`
+--
+
+LOCK TABLES `machine_client` WRITE;
+/*!40000 ALTER TABLE `machine_client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `machine_client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `machines`
+--
+
+DROP TABLE IF EXISTS `machines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `machines` (
+  `machine_id` int(11) NOT NULL,
+  `machine_name` varchar(45) NOT NULL,
+  `reorder_level` int(11) NOT NULL,
+  `stock_limit` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  PRIMARY KEY (`machine_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `machines`
+--
+
+LOCK TABLES `machines` WRITE;
+/*!40000 ALTER TABLE `machines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `machines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proportions`
+--
+
+DROP TABLE IF EXISTS `proportions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proportions` (
+  `rawcoffee_id` int(11) NOT NULL,
+  `blend_id` varchar(45) NOT NULL,
+  `percentage` varchar(45) NOT NULL,
+  PRIMARY KEY (`rawcoffee_id`,`blend_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proportions`
+--
+
+LOCK TABLES `proportions` WRITE;
+/*!40000 ALTER TABLE `proportions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `proportions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `returns`
+--
+
+DROP TABLE IF EXISTS `returns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `returns` (
+  `return_id` int(11) NOT NULL,
+  `return_date` date NOT NULL,
+  `quantity_returned` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `blend_id` int(11) NOT NULL,
+  `remarks` varchar(90) NOT NULL,
+  PRIMARY KEY (`return_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `returns`
+--
+
+LOCK TABLES `returns` WRITE;
+/*!40000 ALTER TABLE `returns` DISABLE KEYS */;
+/*!40000 ALTER TABLE `returns` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supplier` (
+  `supplier_id` int(11) NOT NULL,
+  `supplier_name` varchar(45) NOT NULL,
+  `contact_personnel` varchar(45) NOT NULL,
+  `position` varchar(45) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `email_address` varchar(90) NOT NULL,
+  `telephone_number` varchar(45) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`supplier_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'jhcs_new'
+--
+
+--
+-- Dumping routines for database 'jhcs_new'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-09  2:04:17
