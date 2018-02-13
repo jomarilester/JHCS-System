11969b48f20813683a4ac79c2b50bb5d1f1e86bf
Jomari Lester Julhusin <2131506@slu.edu.ph>
2018-02-13 14:49:44 +0800

    change file extensions to php
    Signed-off-by: Jomari Lester Julhusin <2131506@slu.edu.ph>

---
+++ Admin Module/purchaseOrder.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Purchase Orders</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <!-- Bootstrap core CSS     -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/dataTables.bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/jquery.dataTable.min.css" rel="stylesheet" />
+    <link href="../assets/FileExport/buttons.dataTables.min.css" rel="stylesheet" />
+    <link href="../assets/FileExport/jquery.dataTables.min.css" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="../assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar" data-color="green" data-image="../assets/img/sidebar-1.jpg">
+            <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+        Tip 2: you can also add an image using data-image tag
+    -->
+            <div class="logo ">
+                <img src="../assets/img/logo.png" alt="image1" width="250px" height="150px">
+            </div>
+            <div class="sidebar-wrapper">
+                <ul class="nav">
+                    <li>
+                        <a href="dashboard.php">
+                            <i class="material-icons">dashboard</i>
+                            
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./itemlist.php">
+                            <i class="material-icons">list</i>
+                            <p>Item</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./product_inventory.php">
+                            <i class="material-icons">assessment</i>
+                            <p>Inventory</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./accounts.php">
+                            <i class="material-icons">account_circle</i>
+                            <p>User Accounts</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./clients.php">
+                            <i class="material-icons">person</i>
+                            <p>Clients</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./supplier.php">
+                            <i class="material-icons">person</i>
+                            <p>Suppliers</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./sales_report.php">
+                            <i class="material-icons">library_books</i>
+                            <p>Reports</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-panel">
+            <nav class="navbar navbar-transparent navbar-absolute">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="dropdown">
+                                <li>
+                                    <p class="title">Hi, Player!</p>
+                                </li>
+                                <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
+                                        <i class="material-icons">person</i>
+                                        <p class="hidden-lg hidden-md">Profile</p>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="./user.php">User Profile</a>
+                                    </li>
+                                    <li>
+                                        <a href="./changepassword.php">Change Password</a>
+                                    </li>
+                                    <li>
+                                        <a href="./activitylogs.php">Activity Logs</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Logout</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="content" style="margin-top: -200px;">
+                <div class="container-fluid">
+                    <div class="card">
+                        <div class="card-content">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label">From date:</label>
+                                            <input type="date" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label">To date:</label>
+                                            <input type="date" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </form>
+                        </div>
+                        <div class="card-header" data-background-color="green">
+                            <h4 class="title">Purchase Orders</h4>
+                        </div>
+                        <div class="card-content">
+                            <table id="example" class="display  hover order-column" cellspacing="0" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th><b>Purchase Order No.</b></th>
+                                        <th><b>Supplier</b></th>
+                                        <th><b>Date</b></th>
+                                        <th><b>Product</b></th>
+                                        <th><b>Quantity</b></th>
+                                        <th><b>Amount</b></th>
+                                        <th><b>Trucking Fee</b></th>
+                                        <th><b>Total Amount</b></th>
+                                        <th><b>Remarks</b></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>53</td>
+                                        <td>Lila Enterprise</td>
+                                        <td>Oct 1, 2017</td>
+                                        <td>Arabica Sumatra (City Roast)</td>
+                                        <td>250 Grams</td>
+                                        <td>53,750</td>
+                                        <td>3,500</td>
+                                        <td>57,250</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>54</td>
+                                        <td>La Festive Trading, Inc</td>
+                                        <td>November 25, 2017</td>
+                                        <td>Robusta (Medium Roast)</td>
+                                        <td>300 Grams</td>
+                                        <td>62,500</td>
+                                        <td>3,500</td>
+                                        <td>66,000</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>55</td>
+                                        <td>Lanz Enterprise</td>
+                                        <td>December 17, 2017</td>
+                                        <td>Guatamela (City Roast)</td>
+                                        <td>200 Grams</td>
+                                        <td>44,200</td>
+                                        <td>3,500</td>
+                                        <td>47,700</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<script src="../assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="../assets/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/buttons.flash.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/dataTables.buttons.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/buttons.php5.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/buttons.print.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/jszip.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/pdfmake.min.js" type="text/javascript"></script>
+<script src="../assets/FileExport/vfs_fonts.js" type="text/javascript"></script>
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/js/material.min.js" type="text/javascript"></script>
+<!--  Charts Plugin -->
+<script src="../assets/js/chartist.min.js"></script>
+<!--  Dynamic Elements plugin -->
+<script src="../assets/js/arrive.min.js"></script>
+<!--  PerfectScrollbar Library -->
+<script src="../assets/js/perfect-scrollbar.jquery.min.js"></script>
+<!--  Notifications Plugin    -->
+<script src="../assets/js/bootstrap-notify.js"></script>
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="../assets/js/material-dashboard.js?v=1.2.0"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="../assets/js/demo.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#example').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'excel', 'pdf', 'print'
+        ]
+    });
+});
+</script>
+
+</html>
